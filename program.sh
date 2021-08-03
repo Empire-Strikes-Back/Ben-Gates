2722e87e2db9ee924858296a36ad3330b6606668
wiki-quest <wiki-quest@program>
2021-08-03 21:03:53 +0300

    i revert "for creating uberjar i use genie library"
    This reverts commit 82144ea3ff3cce5ff039611a15bb6d6a8044d05e.

--- program.sh
+++ program.sh
@@ -17,13 +17,4 @@ main(){
     -M -m wiki-quest.main
 }
 
-uberjar(){
-  clj \
-    -X:uberjar genie.core/process \
-    :uberjar-name out/wiki-quest.standalone.jar \
-    :main-ns wiki-quest.main
-  mkdir -p out/jpackage-input
-  mv out/wiki-quest.standalone.jar out/jpackage-input/
-}
-
 "$@"
