d4c257a8b5d19838024989ec9820cbaa6d8f89dd
wiki-quest <wiki-quest@program>
2021-08-03 21:04:12 +0300

    i revert "i use new commit hash of deps-repl - with clj -X"
    This reverts commit 52be1aa5e7bc8a94e36a012f7debc3ed85349b48.

--- program.sh
+++ program.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 repl(){
-  clj \
-    -X:repl deps-repl.core/process \
-    :main-ns wiki-quest.main \
-    :port 7788 \
-    :host '"0.0.0.0"' \
-    :repl? true \
-    :nrepl? false
+  clj -M:repl
 }
-
 
 "$@"
